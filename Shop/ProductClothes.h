6f3864ecfc83758d77f54619fab2d2c38bb83d95
Natalia Pachva <natalia.pachva@gmail.com>
2017-03-06 00:04:41 +0200

    added ProductClothes.cpp

--- Shop/ProductClothes.h
+++ Shop/ProductClothes.h
@@ -6,72 +6,26 @@ using namespace std;
 
 class ProductClothes
 {
-	private:
-		string type;
-		string sex;
-		int size;
-		double price;
-	public:
-		ProductClothes()
-		{
-			type = "T-Shirt";
-			sex = "man";
-			size = 40;
-			price = 100;
-		}
-		ProductClothes(string typeVal, string sexVal, int sizeVal, double priceVal)
-		{
-			type = typeVal;
-			sex = sexVal;
-			size = sizeVal;
-			price = priceVal;
-		}
-		void setType(string newType)
-		{
-			type = newType;
-		}
-		void setSize(int sizeVal)
-		{
-			size = sizeVal;
-		}
-		void setPrice(double priceVal)
-		{
-			price = priceVal;
-		}
-		string getType()
-		{
-			return type;
-		}
-		string getSex()
-		{
-			return sex;
-		}
-		int getSize()
-		{
-			return size;
-		}
-		double getPrice()
-		{
-			return price;
-		}
-		friend istream& operator >> (istream&is, ProductClothes&x)
-		{
-			is >> x.type >> x.sex >> x.size >> x.price;
-			return is;
-		}
-		friend ifstream& operator >> (ifstream&is, ProductClothes&x)
-		{
-			is >> x.type >> x.sex >> x.size >> x.price;
-			return is;
-		}
-		friend ostream& operator<<(ostream&os, const ProductClothes&x)
-		{
-			os << x.type << " " << x.sex << " " << x.size << " " << x.price << endl;
-			return os;
-		}
-		friend ofstream& operator<<(ofstream&os, const ProductClothes&x)
-		{
-			os << x.type << " " << x.sex << " " << x.size << " " << x.price << endl;
-			return os;
-		}
+private:
+	string type;
+	string sex;
+	int size;
+	double price;
+public:
+	ProductClothes();
+	ProductClothes(string typeVal, string sexVal, int sizeVal, double priceVal);
+
+	void setType(string newType);
+	void setSize(int sizeVal);
+	void setPrice(double priceVal);
+	string getType();
+	string getSex();
+	int getSize();
+	double getPrice();
+
+	friend istream& operator >> (istream&is, ProductClothes&x);
+	friend ifstream& operator >> (ifstream&is, ProductClothes&x);
+	friend ostream& operator<<(ostream&os, const ProductClothes&x);
+	friend ofstream& operator<<(ofstream&os, const ProductClothes&x);
 };
+
